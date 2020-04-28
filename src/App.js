be1b06852881bac427ee7b5b5644cddaf317280b
Joanna Krezolek <j.krezolek@gmail.com>
2020-04-28 13:34:25 +0200

    configure prettier$eslint

--- src/App.js
+++ src/App.js
@@ -2,25 +2,35 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit
+        <code>src/App.js</code>
+        and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+        alt="aska"
+      >
+        Dolor sadipscing lorem vero magna dolore et sanctus, ipsum ipsum labore sadipscing sit eos
+        accusam sit, diam dolore rebum nonumy diam, invidunt est aliquyam et et gubergren dolores
+        dolore no stet. No sed amet magna sed stet no et. Kasd et aliquyam amet invidunt, kasd vero
+        dolore no diam dolor et. Justo sanctus consetetur magna et nonumy erat no, stet lorem diam
+        kasd amet, eos et sit tempor justo, sanctus ut consetetur magna diam. Ea amet invidunt eos
+        tempor, duo amet ea justo eirmod stet erat, rebum eirmod vero dolor accusam, et et et ea et
+        est dolore rebum, no vero sit accusam sit dolor, voluptua lorem dolore no duo justo elitr
+        vero et. Dolor eirmod et kasd dolore amet no, amet dolor ut no sit ipsum dolore kasd vero.
+        Gubergren voluptua justo clita duo vero dolor sit kasd. Sadipscing dolor invidunt sanctus
+        et.
+      </a>
+    </header>
+  </div>
+);
 
 export default App;
